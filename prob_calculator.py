10fa272540cb866c2f85b3ffbc3956bc7390660d
ccrsxx <55032197+ccrsxx@users.noreply.github.com>
2021-08-03 13:49:26 +0700

    updating stuffs

--- prob_calculator.py
+++ prob_calculator.py
@@ -1,8 +1,27 @@
 import copy
 import random
-# Consider using the modules imported above.
 
 class Hat:
     
     def __init__(self, **colors):
+        self.contents = [color for color, draw in colors.items() for _ in range(draw)]
 
+    def draw(self, draw):
+        if draw > len(self.contents):
+            return self.contents
+        return [self.contents.pop(random.randrange(len(self.contents))) for _ in range(draw)]
+
+def experiment(hat, num_balls_drawn, num_experiments, **expected_balls):
+    pass
+
+
+x = Hat(red=5, orange=3, black=2)
+
+# shit = {}
+# sample = x.draw(10)
+# for i in sample:
+#     shit[i] = shit.get(i, 0) + 1
+# print('up', shit)
+
+# check = {i: sample.count(i) for i in sample}
+# print(check)
