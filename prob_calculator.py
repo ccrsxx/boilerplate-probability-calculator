bb3382f61d0806768d73481d6f4808be938b6643
ccrsxx <55032197+ccrsxx@users.noreply.github.com>
2021-08-04 11:10:51 +0700

    final

--- prob_calculator.py
+++ prob_calculator.py
@@ -15,6 +15,7 @@ class Hat:
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
     many = 0
     for _ in range(num_experiments):
+        exact = 0
         same = {}
         cls = copy.deepcopy(hat)
         data = cls.draw(num_balls_drawn)
@@ -24,11 +25,8 @@ def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
         if len(same) != len(expected_balls):
             continue
         for k, v in expected_balls.items():
-            exact = False
             if same[k] >= v:
-                exact = True
-            else:
-                break
-        if exact:
+                exact += 1
+        if exact == len(expected_balls):
             many += 1
     return many / num_experiments
